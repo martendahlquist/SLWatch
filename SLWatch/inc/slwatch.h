ee72b2ff23b461f1177d35850e23dcdf5067d2ef
Marten Dahlquist <marten@dahlquist.info>
2018-11-25 11:06:26 +0100

    First commit

---
+++ SLWatch/inc/slwatch.h
@@ -0,0 +1,19 @@
+#ifndef __slwatch_H__
+#define __slwatch_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "slwatch"
+
+#if !defined(PACKAGE)
+#define PACKAGE "info.dahlquist.slwatch"
+#endif
+
+#endif /* __slwatch_H__ */
